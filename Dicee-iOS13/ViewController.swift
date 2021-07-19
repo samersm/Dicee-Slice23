5ce0241e39a7ad64629e00d654c29343cfde6a99
Samer Zagzoog <mrsamer.dev@gmail.com>
2021-07-19 11:37:36 +0300

    Changed User Interface Programmatically

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Who.What = Value
+        
+        // Who          What   Value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
